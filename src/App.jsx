e3f22c54b2fe7dd4a7f4a85c6be8bca4b0b89341
Grima21 <grima2001@gmail.com>
2025-08-21 15:04:49 -0500

    fix: corregir organizacion de carpetas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import { Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
-import Modelos from "./pages/Modelos";
-import ModeloDetalle from "./pages/ModeloDetalle";
-import TestDrive from "./pages/TestDrive";
+// import { Routes, Route } from "react-router-dom";
+import Hero from "./components/Hero";
+import Models from "./components/Models";
+import Features from "./components/Fectures";
 import Footer from "./components/Footer";
-
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/modelos" element={<Modelos />} />
-        <Route path="/modelo/:id" element={<ModeloDetalle />} />
-        <Route path="/test-drive" element={<TestDrive />} />
-      </Routes>
+      <Hero />
+      <Models />
+      <Features />
       <Footer />
     </>
   );
